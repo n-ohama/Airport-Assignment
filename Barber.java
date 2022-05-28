b5a27f4eb906dabaf539d8c22a65599e531c47ac
Wataru <aaaabbbcc@gmail.com>
2022-05-28 16:54:56 +0900

    gggg

--- Barber.java
+++ Barber.java
@@ -1,35 +1,32 @@
-class Barber implements Runnable
-{
+class Barber implements Runnable {
     Bshop shop;
     public boolean closingTime = false;
 
-    public Barber(Bshop shop)
-    {
+    public Barber(Bshop shop) {
         this.shop = shop;
     }
-    public void run()
-    {
-        try
-        {
+
+    public void run() {
+        try {
             Thread.sleep(1000);
-        }
-        catch(InterruptedException iex)
-        {
+        } catch (InterruptedException iex) {
             iex.printStackTrace();
         }
-        System.out.println("Barber started..");
-        while(!closingTime)
-        {
+        System.out.println("Barber started.....");
+        while (!closingTime) {
             shop.cutHair();
         }
-        if (closingTime) { try {
-     			Thread.sleep(5000);
-     		} catch (InterruptedException e) {
-     			// TODO Auto-generated catch block
-     			e.printStackTrace();
-     		} return;}
+        if (closingTime) {
+            try {
+                Thread.sleep(5000);
+            } catch (InterruptedException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+            return;
+        }
     }
-    
+
     public synchronized void setclosingTime() {
         closingTime = true;
         System.out.println("Barbar: We are Closing");
